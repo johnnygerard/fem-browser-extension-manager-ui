099c25648cd013f6494cf3c68940baab815e196b
Johnny Gérard <john@jgerard.dev>
2025-03-23 06:09:36 +0100

    refactor: extract focus ring style

--- src/component/app-switch.tsx
+++ src/component/app-switch.tsx
@@ -12,8 +12,7 @@ export const AppSwitch = memo(({ className }: Props) => {
       className={cn(
         "h-5 w-9 rounded-full bg-neutral-300 p-0.5",
         "data-[state=checked]:bg-red-700 data-[state=checked]:hover:bg-red-500",
-        "transition-[background-color,outline] duration-200 ease-linear",
-        "outline-2 outline-offset-2 outline-transparent focus-visible:outline-red-400",
+        "tw_focus_ring transition-colors duration-200 ease-linear",
         className,
       )}
     >

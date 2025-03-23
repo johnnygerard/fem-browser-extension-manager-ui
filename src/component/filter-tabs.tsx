828d22622b7c6a0d732b00eb1c1145d230a8e741
Johnny Gérard <john@jgerard.dev>
2025-03-23 17:10:58 +0100

    feat: add filter-tabs.tsx

---
+++ src/component/filter-tabs.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/util/cn";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+import { memo } from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const FilterTabs = memo(({ className }: Props) => {
+  return (
+    <RadioGroup.Root className={cn("flex gap-3", className)} defaultValue="All">
+      {["All", "Active", "Inactive"].map((label) => (
+        <RadioGroup.Item
+          className={cn(
+            // Base style
+            "tw_focus_ring rounded-full border px-5 pt-2 pb-2.5",
+            "text-[1.25rem]/[1.4] -tracking-[0.01875rem] transition-colors",
+            // Shadow
+            "shadow-[0_1px_2px_rgba(184,196,215,0.40)] hover:shadow-none",
+            "data-[state=checked]:shadow-none dark:shadow-none",
+            // Text
+            "text-neutral-900 data-[state=checked]:text-neutral-0",
+            "dark:data-[state=unchecked]:text-neutral-0",
+            "data-[state=checked]:font-medium",
+            // Background
+            "bg-neutral-0 data-[state=checked]:bg-red-700",
+            "dark:bg-neutral-700 dark:data-[state=checked]:bg-red-400",
+            "hover:data-[state=checked]:bg-red-500",
+            "hover:dark:bg-neutral-600 hover:dark:data-[state=checked]:bg-red-500",
+            // Border
+            "border-neutral-200 data-[state=checked]:border-transparent",
+            "dark:data-[state=unchecked]:border-neutral-600",
+            // Opacity
+            "hover:data-[state=unchecked]:opacity-70 dark:opacity-100",
+          )}
+          key={label}
+          value={label}
+        >
+          {label}
+        </RadioGroup.Item>
+      ))}
+    </RadioGroup.Root>
+  );
+});
+
+FilterTabs.displayName = "FilterTabs";

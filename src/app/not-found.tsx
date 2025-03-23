79af49bba68dd70882920425860776d5d9cc2a4c
Johnny Gérard <john@jgerard.dev>
2025-03-23 12:17:12 +0100

    feat: style not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/util/cn";
 import { Metadata } from "next";
 import Link from "next/link";
 import { memo } from "react";
@@ -8,12 +9,22 @@ export const metadata: Metadata = {
 
 const NotFound = () => {
   return (
-    <div className="grid min-h-screen place-items-center">
-      <div className="text-center">
-        <h1>404 Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link href="/">Back to Home</Link>
-      </div>
+    <div className="mt-20 flex flex-col items-center text-center">
+      <h1 className="mb-4 text-4xl font-bold text-gray-800 dark:text-gray-100">
+        404 Not Found
+      </h1>
+      <p className="mb-8 text-lg text-gray-600 dark:text-gray-300">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <Link
+        href="/"
+        className={cn(
+          "tw_focus_ring rounded px-6 py-3 font-medium text-white",
+          "bg-blue-500 transition-colors hover:bg-blue-600",
+        )}
+      >
+        Back to Home
+      </Link>
     </div>
   );
 };

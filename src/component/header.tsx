603b9d68f3dea5616a222323a821409d54e79238
Johnny Gérard <john@jgerard.dev>
2025-03-23 07:05:15 +0100

    feat: add header.tsx

---
+++ src/component/header.tsx
@@ -0,0 +1,27 @@
+import { Logo } from "@/component/logo";
+import { ThemeToggle } from "@/component/theme-toggle";
+import { cn } from "@/util/cn";
+import { memo } from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const Header = memo(({ className }: Props) => {
+  return (
+    <header
+      className={cn(
+        "flex justify-between gap-3 rounded-10 px-3 py-2",
+        "border border-neutral-200 dark:border-none",
+        "bg-neutral-0 dark:bg-neutral-800",
+        "shadow-[0_2px_3px_#D9E5F4] dark:shadow-none",
+        className,
+      )}
+    >
+      <Logo />
+      <ThemeToggle />
+    </header>
+  );
+});
+
+Header.displayName = "Header";

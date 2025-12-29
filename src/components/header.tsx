54060c76004fec363637725bbd26ca231f9bef1a
Johnny Gérard <john@jgerard.dev>
2025-12-29 21:34:32 +0100

    refactor: migrate Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,23 @@
-import { Logo } from "@/component/logo";
-import { ThemeToggle } from "@/component/theme-toggle";
-import { cn } from "@/util/cn";
-import { memo } from "react";
+import type { FC } from "react";
+import { Logo } from "~/components/logo";
+import { ThemeToggle } from "~/components/theme-toggle";
+import { tw } from "~/utils/tw";
 
 type Props = {
   className?: string;
 };
 
-export const Header = memo(({ className }: Props) => {
-  return (
-    <header
-      className={cn(
-        "flex justify-between gap-3 rounded-10 px-3 py-2",
-        "border border-neutral-200 dark:border-transparent",
-        "bg-neutral-0 dark:bg-neutral-800",
-        "shadow-[0_2px_3px_#D9E5F4] dark:shadow-none",
-        className,
-      )}
-    >
-      <Logo />
-      <ThemeToggle />
-    </header>
-  );
-});
-
-Header.displayName = "Header";
+export const Header: FC<Props> = ({ className }) => (
+  <header
+    className={tw(
+      "flex justify-between gap-3 rounded-10 px-3 py-2",
+      "border border-neutral-200 dark:border-transparent",
+      "bg-neutral-0 dark:bg-neutral-800",
+      "shadow-[0_2px_3px_#D9E5F4] dark:shadow-none",
+      className,
+    )}
+  >
+    <Logo />
+    <ThemeToggle />
+  </header>
+);
